15a1a1af7a479f481a2b4c2e5bfbad342200b5ae
Shatunov Evgeny <shatunov868@gmail.com>
2025-06-05 19:24:17 +0300

    chore: Update config

--- src/model/hparams.py
+++ src/model/hparams.py
@@ -1,8 +1,8 @@
 config = dict(
     gamma = 0.99,  # discount factor
-    epsilon_min = 0.01,
+    epsilon_min = 0.1,
     epsilon_decay = 0.998,
-    learning_rate = 0.0005,
+    learning_rate = 0.001,
     batch_size = 64,
     update_target_freq = 1000,
     deque_maxlen = 2000,
